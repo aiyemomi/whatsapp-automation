22e693099c8d5a0a21b6e56b93148407777a86b1
aiyemomi <87931564+aiyemomi@users.noreply.github.com>
2022-12-26 17:23:01 +0100

    Update days.js

--- modules/days.js
+++ modules/days.js
@@ -1,14 +1,12 @@
-// Boiler plate code to be able to both require and import modules
+
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const fs = require ('fs');
 // ---------------------
 
-
-// function to calculate the number of days between "2022-10-29"(the date code was deployed) and today's date
 let currentDate = new Date().toJSON().slice(0, 10);
 
-export let numOfDays = dayDifference("2022-10-29", currentDate);
+export let numOfDays = dayDifference("2022-12-26", currentDate);
 export default function dayDifference(firstDate, secondDate) {
   firstDate = new Date(firstDate);
   secondDate = new Date(secondDate);
